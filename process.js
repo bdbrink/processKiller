63174c8aa246205df7c69128153c048d2351c0ba
bdbrink <brendan.brink@outlook.com>
2023-07-24 17:15:45 -0500

    fix comment formmating

--- process.js
+++ process.js
@@ -1,7 +1,7 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 function getProcessList() {
-  const command = 'ps aux';
+  const command = "ps aux";
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
@@ -13,26 +13,32 @@ function getProcessList() {
       return;
     }
 
-    console.log('Running Processes:');
+    console.log("Running Processes:");
     console.log(stdout);
 
-    const excludeUsers = ['root', 'e143608']; // Add the users you want to exclude here
+    // Add the users you want to exclude here
+    const excludeUsers = ["root", "e143608"];
     const unusedProcesses = findUnusedProcesses(stdout, excludeUsers);
-    console.log('Unused Processes:');
+    console.log("Unused Processes:");
     console.log(unusedProcesses);
   });
 }
 
 function findUnusedProcesses(psOutput, excludeUsers) {
-  const lines = psOutput.split('\n').slice(1); // Remove the header line
+  // Remove the header line
+  const lines = psOutput.split("\n").slice(1);
 
   const unusedProcesses = [];
   for (const line of lines) {
     const [user, pid, cpu, mem, command] = line.trim().split(/\s+/);
 
     // Check conditions to consider a process as unused (you can modify these conditions as per your requirement)
-    const isUnused = cpu === '0.0' && mem === '0.0' && !excludeUsers.includes(user) && !command.includes('ps aux');
-    
+    const isUnused =
+      cpu === "0.0" &&
+      mem === "0.0" &&
+      !excludeUsers.includes(user) &&
+      !command.includes("ps aux");
+
     if (isUnused) {
       unusedProcesses.push({
         user,
